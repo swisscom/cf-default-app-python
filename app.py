32356ef4b99adc344aaa594372faa30bf717a064
Tobias Fuhrimann <tobias.fuhrimann@swisscom.com>
2017-06-09 13:11:36 +0200

    Migrate to SDX

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
-# import dependencies
 import os
 from flask import Flask, request, render_template
 
-# bootstrap the app
+# Bootstrap the app
 app = Flask(__name__)
 
-# set the port dynamically with a default of 3000 for local development
+# Set the port dynamically with a default of 3000 for local development
 port = int(os.getenv('PORT', '3000'))
 
-# our base route which just returns a string
+# Our base route which renders an HTML template
 @app.route('/')
 def index():
     return render_template('index.html', env_vars=os.environ, req_headers=request.headers, req_params=request.args)
 
-# start the app
+# Start the app
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
